04f647e9dc7c7e4a9b0c4b882f2341044c0f88f8
Ayush <ayushcshah@gmail.com>
2024-11-03 16:49:19 -0600

    Initial commit

---
+++ Flickr Search/Flickr_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_SearchApp.swift
+//  Flickr Search
+//
+//  Created by Ayush Shah on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
